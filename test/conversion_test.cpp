e49ce378725edd20a402fd63854e13e43bd0df79
hth <huatenghuiyyds@163.com>
2025-04-02 12:07:57 +0800

    Test All NumSlots

--- test/conversion_test.cpp
+++ test/conversion_test.cpp
@@ -87,7 +87,7 @@ void repack_test(uint32_t scale_bits, size_t num_slots)
 
 int main()
 {
-    for(uint32_t num_slots = (1 << 0); num_slots <= (1 << 6); num_slots <<= 1) {
+    for(uint32_t num_slots = (1 << 0); num_slots < (1 << 16); num_slots <<= 1) {
         std::cout << "<<<<<<<<<<<<============ Num Slots: " << num_slots << " ============>>>>>>>>>>>>" << std::endl;
         repack_test(29, num_slots);
     }
